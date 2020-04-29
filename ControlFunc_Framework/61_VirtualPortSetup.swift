3053f1cf64a6361f5aafde5628f54aad8118fc81
M-Hachi <emoriuchi08@gmail.com>
2020-04-29 15:40:53 +0900

    made class

--- ControlFunc_Framework/61_VirtualPortSetup.swift
+++ ControlFunc_Framework/61_VirtualPortSetup.swift
@@ -2,10 +2,6 @@
 //  VirtualPortSetup.swift
 //  Control-Function
 //
-//  Created by 森内　映人 on 2020/04/04.
-//  Copyright © 2020 森内　映人. All rights reserved.
-//
-
 import Foundation
 import CoreBluetooth
 
